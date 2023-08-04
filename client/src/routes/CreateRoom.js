efce4b2c90235deb99ea7ebf36340fde1ea32329
shyambaseapp <shyam.baseapp@gmail.com>
2023-08-04 12:06:38 +0530

    initial commit

---
+++ client/src/routes/CreateRoom.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { v1 as uuid } from "uuid";
+
+const CreateRoom = (props) => {
+  function create() {
+    const id = uuid();
+    props.history.push(`/room/${id}`);
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="d-grid gap-2 col-2">
+        <button
+          type="button"
+          className="btn btn-outline-primary btn-lg"
+          onClick={create}
+        >
+          Create Meeting
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CreateRoom;
