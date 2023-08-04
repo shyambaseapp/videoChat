05523d20449b6560569f6cc158e84c3d7647f50e
shyambaseapp <shyam.baseapp@gmail.com>
2023-08-04 15:20:37 +0530

    adding script

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -41,7 +41,7 @@ const Room = (props) => {
   const roomID = props.match.params.roomID;
 
   useEffect(() => {
-    socketRef.current = io.connect("https://videochatserver-evr8.onrender.com:3000");
+    socketRef.current = io.connect("https://videochatserver-evr8.onrender.com:8000");
     navigator.mediaDevices
       .getUserMedia({ video: videoConstraints, audio: true })
       .then((stream) => {
