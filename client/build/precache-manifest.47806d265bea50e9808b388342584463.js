0d6abc7b594088c18e2681edc2dfde8ee54bc5b3
shyambaseapp <shyam.baseapp@gmail.com>
2023-08-08 12:50:26 +0530

    adding script

---
+++ client/build/precache-manifest.47806d265bea50e9808b388342584463.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9bc08a6dcb40c52cda812176a28f6fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b5fad8d36314f0292d7",
+    "url": "/static/js/2.ac4c030e.chunk.js"
+  },
+  {
+    "revision": "c12e4b25532e192eac4a9ddfe6392d28",
+    "url": "/static/js/2.ac4c030e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f70d0dd48776a317e8e",
+    "url": "/static/js/main.b3674cfe.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -4,7 +4,7 @@ import { v1 as uuid } from "uuid";
 const CreateRoom = (props) => {
   function create() {
     const id = uuid();
-    window.location.href = `/room/${id}`;
+    props.history.push(`/room/${id}`);
   }
 
   return (
